a0fc50ec35570bf84fc00226db52928f78b03253
ninabelikova <daimnevodka@gmail.com>
2015-10-05 19:32:15 +0800

    Add a full-title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  # Return the full title on a page basis
+  def full_title(page_title = '')
+    base_title = "The Magnificent Hababa"
+    if page_title.empty?
+      base_title
+    else
+      page_title + '|' + base_title
+    end
 end
