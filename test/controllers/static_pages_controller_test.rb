7209e6291c6c2ef98487d9db9e11b6d85f659796
ninabelikova <daimnevodka@gmail.com>
2015-10-05 18:57:00 +0800

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | The Magnificent Hababa"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | The Magnificent Hababa"
+  end
+
+  test "should get about" do
+      get :about
+      assert_response :success
+      assert_select "title", "About | The Magnificent Hababa"
   end
 
 end
